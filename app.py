5cca0a73e21a4a4f95d5aa9fe8b76448115552a4
Joseesoto <38842863+Joseesoto@users.noreply.github.com>
2025-07-17 15:03:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    mensaje = "✅ Conexión entre app.py y HTML funcionando desde Render"
-    return render_template("index.html", mensaje=mensaje)
+    # Pares simulados para prueba
+    rows = [
+        {
+            "exchange": "Binance",
+            "baseCurrency": "BTC",
+            "quoteCurrency": "USDT",
+            "symbol": "BTC/USDT",
+            "price": 30150.57,
+            "link": "https://www.binance.com/en/trade/BTC_USDT"
+        },
+        {
+            "exchange": "Binance",
+            "baseCurrency": "ETH",
+            "quoteCurrency": "USDC",
+            "symbol": "ETH/USDC",
+            "price": 1875.22,
+            "link": "https://www.binance.com/en/trade/ETH_USDC"
+        },
+        {
+            "exchange": "Binance",
+            "baseCurrency": "SOL",
+            "quoteCurrency": "BUSD",
+            "symbol": "SOL/BUSD",
+            "price": 22.43,
+            "link": "https://www.binance.com/en/trade/SOL_BUSD"
+        }
+    ]
+    return render_template("index.html", rows=rows)
 
 if __name__ == '__main__':
     app.run(debug=True)
